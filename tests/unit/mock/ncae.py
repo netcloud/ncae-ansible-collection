d61fa89441add1a888acd69b46c8c8936209a282
Pascal Mathis <mathis@netcloud.ch>
2023-02-21 17:11:44 +0100

    Add unit and integration testing to collection

---
+++ tests/unit/mock/ncae.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023 Netcloud AG
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+import json
+
+from ansible_collections.netcloud.ncae.plugins.module_utils.ncae import (
+    NcaeClient,
+    NcaeSession,
+)
+
+
+class MockNcaeClient:
+    def __init__(self, mocker, default_mocks=True):
+        # Prepare state for handling mocked calls
+        self._mocker = mocker
+        self._client = None
+        self._handlers = {}
+
+        # Initialize with default mocks unless disabled
+        if default_mocks:
+            self._load_default_mocks()
+
+    def mock_fixture(self, method, url, name):
+        with open(f"tests/unit/fixtures/{name}.json", "rb") as fp:
+            fixture = json.load(fp)
+            handler_key = self._get_handler_key(method, url)
+            self._handlers[handler_key] = lambda **_: fixture
+
+    def __getattr__(self, attr):
+        # Build mocked client on-demand instead of during class construction.
+        # At least one mock for the login endpoint is required.
+        if self._client is None:
+            self._client = self._build_mock_client()
+
+        # Passthrough to mocked client so all methods are supported.
+        return getattr(self._client, attr)
+
+    def _load_default_mocks(self):
+        # List of URLs to map to static JSON fixtures for testing
+        fixtures = [
+            ["POST", "auth", "v1", "login"],
+            ["GET", "dashboard", "v1", "device-group"],
+            ["GET", "dashboard", "v1", "simple-device"],
+        ]
+
+        # Create new fixture-based request mock for all urls
+        for url_parts in fixtures:
+            method = url_parts.pop(0)
+            url = "/api/" + "/".join(url_parts)
+            fixture = method.lower() + "-" + "-".join(url_parts)
+            self.mock_fixture(method, url, fixture)
+
+    def _handle_mock_request(self, method, url, **kwargs):
+        handler_key = self._get_handler_key(method, url)
+        handler = self._handlers.get(handler_key)
+        if handler is None:
+            raise Exception("No request mapping for " + method + " " + url)
+
+        return handler(method=method, url=url)
+
+    def _get_handler_key(self, method, url):
+        return (method.upper(), url.rstrip("/"))
+
+    def _build_mock_client(self):
+        # Mock _request method to catch all external API requests.
+        # These will be dispatched to this class instead for per-request mocking.
+        with self._mocker.patch.object(
+            target=NcaeSession,
+            attribute="_request",
+            side_effect=self._handle_mock_request,
+        ):
+            return NcaeClient(
+                base_url="http://localhost:8080",
+                username="admin",
+                password="admin",
+            )
---
+++ tests/unit/plugins/inventory/test_devices.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023 Netcloud AG
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+import json
+import pytest
+
+from ansible.inventory.data import InventoryData
+from ansible_collections.netcloud.ncae.plugins.inventory.devices import InventoryModule
+from ansible_collections.netcloud.ncae.tests.unit.mock.ncae import MockNcaeClient
+
+
+@pytest.fixture(scope="module")
+def inventory():
+    r = InventoryModule()
+    r.inventory = InventoryData()
+    return r
+
+
+def load_fixture(name):
+    with open(f"tests/unit/plugins/inventory/fixtures/{name}.json", "rb") as fp:
+        return json.load(fp)
+
+
+def mock_options(options):
+    return lambda key: options.get(key, False)
+
+
+def test_populate(inventory, mocker):
+    # Manually specify module options
+    inventory._device_prefix = "ncae_device_"
+    inventory._group_prefix = "ncae_group_"
+    inventory._facts_prefix = "ncae_"
+
+    # Mock ncae client and populate inventory
+    inventory._client = MockNcaeClient(mocker)
+    inventory._populate()
+
+    # Gather different devices from inventory
+    device_multi = inventory.inventory.get_host("ncae_device_1")
+    device_global = inventory.inventory.get_host("ncae_device_2")
+    device_basel = inventory.inventory.get_host("ncae_device_3")
+    device_bern = inventory.inventory.get_host("ncae_device_4")
+    device_winterthur = inventory.inventory.get_host("ncae_device_5")
+
+    # Ensure expected groups are present
+    assert "ncae_group_offices" in inventory.inventory.groups
+    assert "ncae_group_offices__basel" in inventory.inventory.groups
+    assert "ncae_group_offices__bern" in inventory.inventory.groups
+    assert "ncae_group_offices__winterthur" in inventory.inventory.groups
+
+    # Gather different groups from inventory
+    group_global = inventory.inventory.groups["ncae_group_offices"]
+    group_basel = inventory.inventory.groups["ncae_group_offices__basel"]
+    group_bern = inventory.inventory.groups["ncae_group_offices__bern"]
+    group_winterthur = inventory.inventory.groups["ncae_group_offices__winterthur"]
+
+    # Ensure appropriate device memberships
+    assert group_global.hosts == [device_multi, device_global]
+    assert group_basel.hosts == [device_multi, device_basel]
+    assert group_bern.hosts == [device_multi, device_bern]
+    assert group_winterthur.hosts == [device_winterthur]
+
+    # Gather variables for one of the devices
+    vars_global = device_global.get_vars()
+
+    # Ensure device facts are present
+    assert vars_global["ansible_host"] == "192.168.254.2"
+    assert vars_global["ncae_device_id"] == 2
+    assert vars_global["ncae_device_name"] == "device-global"
